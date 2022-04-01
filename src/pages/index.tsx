5848f542fd7d117495237b43112ed3394ed354c1
pq43 <51523958+pq43@users.noreply.github.com>
2022-04-01 09:29:02 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
               <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
                  <li className="nav-item">
                   <a
-                    className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    className="nav-link block pr-2 lg:px-2 py-2 text-pink-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
                     href="#!"
                     data-mdb-ripple="true"
                     data-mdb-ripple-color="light"
