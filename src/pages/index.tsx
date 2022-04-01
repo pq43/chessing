21aa706901102ee23e2846cd2f6e3bc09ecd20b5
pq43 <51523958+pq43@users.noreply.github.com>
2022-04-01 09:26:57 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,16 @@ const Home: NextPage = () => {
               id="navbarSupportedContentY"
             >
               <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
+                 <li className="nav-item">
+                  <a
+                    className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
+                    href="#!"
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    Chessing
+                  </a>
+                </li>
                 <li className="nav-item">
                   <a
                     className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out"
