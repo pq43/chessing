1d4dbb0b92b2fe515bb34a4358bae552e8c2a019
pq43 <shugis2op@gmail.com>
2022-04-14 23:36:02 -0700

    a lot of changes

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,8 +4,8 @@ import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
+  { name: 'Home', href: '/', current: false },
+  { name: 'Dashboard', href: 'dashboard', current: false },
   { name: 'Projects', href: '#', current: false },
   { name: 'Calendar', href: '#', current: false },
 ];
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,14 +9,19 @@ const Home = () => {
       <div className="hero min-h-screen bg-[url('https://cdn.discordapp.com/attachments/943206869856571432/959256017491873802/unknown.png')]">
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+          <div className="max-w-5xl">
+            <h1 className="mb-5 text-8xl font-bold text-white">
+              Improve your chess game{' '}
+              <span className="italic text-pink-300"> with finesse.</span>
+            </h1>
+            <p className="mb-5 text-white text-3xl">
+              Don&apos;t hold yourself back from the best techniques available
+              for learning chess. Use Chessing and start training like the pros
+              immediately.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <button className="bg-purple-400 hover:bg-purple-500 text-white font-bold py-8 px-16 rounded-full inline-flex items-center text-3xl my-10">
+              <span>Let&apos;s get started</span>
+            </button>
           </div>
         </div>
       </div>
