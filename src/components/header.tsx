185e90c5eb7d175b800ac6d2ab7de6f9f3856e55
pq43 <51523958+pq43@users.noreply.github.com>
2022-05-25 20:50:32 -0700

    Update header.tsx

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,9 +5,6 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline';
 
 const navigation = [
   { name: 'Home', href: '/', current: false },
-  { name: 'Dashboard', href: 'dashboard', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
 ];
 
 function classNames(...classes) {
@@ -34,16 +31,7 @@ export default function Header() {
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex-shrink-0 flex items-center">
-                  <img
-                    className="block lg:hidden h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                    alt="Workflow"
-                  />
-                  <img
-                    className="hidden lg:block h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
-                    alt="Workflow"
-                  />
+                  Chessing
                 </div>
                 <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
